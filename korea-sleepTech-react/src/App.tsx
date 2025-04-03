b86c2fcb841bc1a16e04d1cdaba18def0298d96c
iasos2 <ykkminji@gmail.com>
2025-04-03 10:58:59 +0900

    Update App.tsx

--- korea-sleepTech-react/src/App.tsx
+++ korea-sleepTech-react/src/App.tsx
@@ -7,6 +7,9 @@ import './App.css'
 //  >> index.tsx 파일은 해당 파일이 포함된 폴더의 메인 파일로 인식!
 //  >> import 시 폴더명 만으로 가져오기 가능!
 import Basic from '@/pages/a_basic/index'; // '@/pages/a_basic/index.tsx'와 동일
+import Hooks from '@/pages/b_hooks';
+import Router from '@/pages/c_router';
+import NaviBar from './components/NaviBar';
 
 //& 기본 Vite React 앱의 경로
 // : http://localhost:5173
@@ -14,6 +17,8 @@ function App() {
   return (
     <>
       <h1>Korea SleepTech React</h1>
+      <NaviBar />
+
       {/* Routes 태그: Route를 감싸는 컴포넌트 */}
       <Routes>
         {/* Route 태그: 단일 태그 사용 권장! */}
@@ -22,6 +27,15 @@ function App() {
           http://localhost:5173/basic
         */}
         <Route path='/basic' element={<Basic />} />
+        <Route path='/hooks' element={<Hooks />} />
+
+        {/* 
+          중첩 라우팅
+          : 해당 컴포넌트의 경로 내부에서 라우트 경로에 따라 페이지 전환이 일어남을 명시
+
+          path='/해당컴포넌트기본경로/*'
+        */}
+        <Route path='/router/*' element={<Router />} />
       </Routes>
     </>
   )
