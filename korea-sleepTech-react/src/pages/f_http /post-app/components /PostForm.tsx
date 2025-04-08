6b9e22434c8abd1ed9fb54446153dc11e6f39b13
iasos2 <ykkminji@gmail.com>
2025-04-08 09:59:12 +0900

    Create PostForm.tsx

---
+++ korea-sleepTech-react/src/pages/f_http /post-app/components /PostForm.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Post } from "../types/Post";
+import postApi from "../apis/postApi";
+
+//! === 글 작성/수정 폼 ===
+function PostForm() {
+  const [inputValue, setInputValue] = useState<Post>({
+    title: "",
+    body: "",
+  });
+  const { title, body } = inputValue;
+
+  const storedId = localStorage.getItem("editingPostId");
+
+  const handleInputValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+
+    //? 화살표 함수 사용할 때 객체 반환 시 반드시 ()소괄호로 감싸서 반환!
+    setInputValue(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async () => {
+    try {
+      if (storedId) {
+        // 수정(UPDATE)
+
+      } else {
+        // 생성(CREATE)
+        await postApi.post("/posts", { title, body });
+        alert("등록 완료")
+      }
+
+    } catch (error) {
+      console.error("요청 실패: ", error);
+      alert("오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  }
+
+  return (
+    <div>
+      <h2>{storedId ? "게시글 수정" : "게시글 작성"}</h2>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={handleInputValueChange}
+        placeholder="제목"
+      />
+      <br />
+      <textarea 
+        name="body"
+        value={body} 
+        onChange={handleInputValueChange} 
+        placeholder="내용" 
+      />
+      <br />
+      <button onClick={handleSubmit}>{storedId ? "수정하기" : "등록하기"}</button>
+    </div>
+  );
+}
+
+export default PostForm;
